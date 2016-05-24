8d979d5cf2f011fd7bc4f00284eef0d76a4b75d3
anton <tony.potapov@gmail.com>
2016-05-24 17:16:32 +0300

    bug fixed in app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.posts = [
+	  {title: 'post1', upvotes: 5},
+	  {title: 'post2', upvotes: 2},
+	  {title: 'post3', upvotes: 15},
+	  {title: 'post4', upvotes: 9},
+	  {title: 'post5', upvotes: 4}
+  ];
 }]);
 
-$scope.posts = [
-  {title: 'post1', upvotes: 5},
-  {title: 'post2', upvotes: 2},
-  {title: 'post3', upvotes: 15},
-  {title: 'post4', upvotes: 9},
-  {title: 'post5', upvotes: 4}
-];
+
