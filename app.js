30dedcd873614bd6342d92fcbb53b23306cf98c1
anton <tony.potapov@gmail.com>
2016-05-24 17:36:27 +0300

    add title to post

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ function($scope){
 	  {title: 'post5', upvotes: 4}
   ];
   $scope.addPost = function() {
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
   };
 }]);
 
