88b2685ecbedfcd51f39e372f22594fc7735d950
anton <tony.potapov@gmail.com>
2016-05-24 16:44:41 +0300

    fix bag in app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ function($scope){
 
 $scope.posts = [
   {title: 'post1', upvotes: 5},
-  {title: 'post2', upvotes: 2}
-  {title: 'post3', upvotes: 15}
-  {title: 'post4', upvotes: 9}
+  {title: 'post2', upvotes: 2},
+  {title: 'post3', upvotes: 15},
+  {title: 'post4', upvotes: 9},
   {title: 'post5', upvotes: 4}
 ];
