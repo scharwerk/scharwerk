bd4f483be0f222829bc8c1874a941391edf07aae
anton <tony.potapov@gmail.com>
2016-05-25 15:18:20 +0300

    add submit link function

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ function($scope){
   ];
   $scope.addPost = function() {
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0});
     $scope.title = '';
+    $scope.link = '';
   };
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
