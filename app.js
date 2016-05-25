f8941eca661d34a1ed78cfeed7cd95915cc72097
anton <tony.potapov@gmail.com>
2016-05-25 14:50:46 +0300

    add upvote function

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ function($scope){
     $scope.posts.push({title: $scope.title, upvotes: 0});
     $scope.title = '';
   };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
 
 
