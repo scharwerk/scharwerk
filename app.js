4e1c4c5e891f255759919412f04c027c7ccdd0ed
anton <tony.potapov@gmail.com>
2016-06-01 16:49:06 +0300

    add ui-router library

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
 .factory('posts', [function(){
   var o = {
     posts: []
