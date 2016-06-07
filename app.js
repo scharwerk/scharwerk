585b4dfc58724607e4d08c3573be6661171f64ee
anton <tony.potapov@gmail.com>
2016-06-07 14:45:52 +0300

    create addComment fuction

--- app.js
+++ app.js
@@ -58,4 +58,14 @@ function($scope, posts){
 'posts',
 function($scope, $stateParams, posts){
   $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }]);
