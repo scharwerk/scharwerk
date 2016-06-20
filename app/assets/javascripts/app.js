ff3f15f3f08e38976cef7accbca3a5db05c6bd58
anton <tony.potapov@gmail.com>
2016-06-20 15:57:31 +0300

    move controller postsCtrl from app.js to posts.Ctrl

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,21 +19,3 @@ function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('home');
 }])
-
-.controller('PostsCtrl', [
-'$scope',
-'$stateParams',
-'posts',
-function($scope, $stateParams, posts){
-  $scope.post = posts.posts[$stateParams.id];
-
-  $scope.addComment = function(){
-    if($scope.body === '') { return; }
-    $scope.post.comments.push({
-      body: $scope.body,
-      author: 'user',
-      upvotes: 0
-    });
-    $scope.body = '';
-  };
-}]);
---
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -0,0 +1,18 @@
+angular.module('flapperNews')
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}]);
