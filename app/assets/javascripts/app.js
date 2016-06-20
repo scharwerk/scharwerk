e8f13413660e3cfcd5faefca1e8466a5d16b2fba
anton <tony.potapov@gmail.com>
2016-06-20 15:52:23 +0300

    move mainCtrl controller from app.js to mainCtrl.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -20,31 +20,6 @@ function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('home');
 }])
 
-.controller('MainCtrl', [
-'$scope',
-'posts',
-function($scope, posts){
-  $scope.posts = posts.posts;
-  $scope.addPost = function() {
-    if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0},
-
-      ]
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
-}])
-
 .controller('PostsCtrl', [
 '$scope',
 '$stateParams',
