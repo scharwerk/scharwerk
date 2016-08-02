95e5f8f5df6858302fd1226dcb4a245a421f5d6d
anton <tony.potapov@gmail.com>
2016-08-02 13:33:48 +0300

    add respond_to :json to application controller

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'angular-rails-templates'
 
+gem 'responders'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  respond_to :json
+  
   def angular
     render 'layouts/application'
   end
