d566d3201fb267bdc948ebac83c48262d3eb56b8
anton <tony.potapov@gmail.com>
2016-08-05 12:35:23 +0300

    modify .addPost() method to save posts to the server

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -6,15 +6,9 @@ function($scope, posts){
   $scope.posts = posts.posts;
   $scope.addPost = function() {
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
+    posts.create({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0},
-
-      ]
     });
     $scope.title = '';
     $scope.link = '';
