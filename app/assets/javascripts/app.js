c180d2d72d7f5bc99f13972c54a027c03d06dc73
anton <tony.potapov@gmail.com>
2016-08-10 14:58:14 +0300

    add the resolve object to posts state

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -20,6 +20,11 @@ function($stateProvider, $urlRouterProvider) {
       url: '/posts/{id}',
       templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
     });
 
   $urlRouterProvider.otherwise('home');
