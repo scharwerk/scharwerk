3ed6286eddcd9f354cb9e9612daf4db11fff8d48
anton <tony.potapov@gmail.com>
2016-08-10 15:06:55 +0300

    modify controller to gain access to the post object

--- app/assets/javascripts/posts/postsCtrl.js
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -1,10 +1,10 @@
 angular.module('flapperNews')
 .controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
-  $scope.post = posts.posts[$stateParams.id];
+'post',
+function($scope, posts, post){
+  $scope.post = post;
 
   $scope.addComment = function(){
     if($scope.body === '') { return; }
