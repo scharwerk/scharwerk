907a04d9981dd2bad2f201e09a63882885ad3108
anton <tony.potapov@gmail.com>
2016-08-13 19:47:25 +0300

    fix typo in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parametr_senitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end
