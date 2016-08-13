0f64a5a82c9d33eb5bb494c7261e4b001944eff3
anton <tony.potapov@gmail.com>
2016-08-13 20:12:24 +0300

    update create action  in CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   
   def create
     post = Post.find(params[:post_id])
-    comment = post.comments.create(comment_params)
+    comment = post.comments.create(comment_params.merge(user_id: current_user.id))
     respond_with post, comment
   end
 
