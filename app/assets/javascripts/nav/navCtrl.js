f409ed1994662a5bfb6a77427dd6dc16f31abf24
anton <tony.potapov@gmail.com>
2016-08-13 18:17:20 +0300

    add event listeners to handle when user authenticate and logs out

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -10,4 +10,17 @@ function($scope, Auth){
   Auth.currentUser().then(function (user){
   	$scope.user = user;
   });
+
+  $scope.$on('devise:new-registration', function (e, user){
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:login', function (e, user){
+  	$scope.user = user;
+  });
+
+  $scope.$on('devise:logout', function (e, user){
+  	$scope.user = {};
+  });
+  
 }]);
