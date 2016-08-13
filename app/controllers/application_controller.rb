af897eebee40ee0c8ed4d87157f4f4ed33a37779
anton <tony.potapov@gmail.com>
2016-08-13 19:39:06 +0300

    add username to allowed parametrs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   def angular
     render 'layouts/application'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parametr_senitizer.for(:sign_up) << :username
   end
 end
