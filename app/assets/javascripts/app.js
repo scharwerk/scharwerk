8c5b237ef6c8d793ec9d1a07e376c6f5678ffd58
anton <tony.potapov@gmail.com>
2016-08-13 14:11:17 +0300

    fix typo in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,7 +19,7 @@ function($stateProvider, $urlRouterProvider) {
     .state('posts', {
       url: '/posts/{id}',
       templateUrl: 'posts/_posts.html',
-      controller: 'PostsCtrl'
+      controller: 'PostsCtrl',
       resolve: {
         post: ['$stateParams', 'posts', function($stateParams, posts) {
           return posts.get($stateParams.id);
