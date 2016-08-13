280adf2dd9a30a80eb3883cdf101d73f1d32128c
anton <tony.potapov@gmail.com>
2016-08-13 17:23:58 +0300

    inject Devise module into app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui.router', 'templates'])
+angular.module('flapperNews', ['ui.router', 'templates', 'Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require angular
-//= require angular-devise
+//= require AngularDevise 
 //= require angular-rails-templates
 //= require angular-ui-router
 //= require_tree .
