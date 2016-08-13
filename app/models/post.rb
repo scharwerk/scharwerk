01509a03a1580e696ca153fd5e6d7b24f0dce57d
anton <tony.potapov@gmail.com>
2016-08-13 20:18:20 +0300

    update as_json method in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   def as_json(options = {})
-  	super(options.merge(include: :comments))
+  	super(options.merge(include: [:user, comments: {include: :user}]))
   end
 end
