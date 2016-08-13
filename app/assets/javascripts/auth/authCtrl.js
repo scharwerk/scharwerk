eb9086592af361825625b65e0f8da2af7756b522
anton <tony.potapov@gmail.com>
2016-08-13 18:31:22 +0300

    add login adn register functions

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -4,5 +4,16 @@ angular.module('flapperNews')
 '$state',
 'Auth',
 function($scope, $state, Auth){
+  $scope.login = function() {
+  	Auth.login($scope.user).then(function(){
+  		$state.go('home');
+  	});
+  };
+
+  $scope.register = function() {
+  	Auth.register($scope.user).then(function(){
+  		$state.go('home');
+  	});
+  };
 }]);
 
