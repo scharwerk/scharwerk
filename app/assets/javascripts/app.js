5cb2c6eacfe5a4b1feda9ff2b673fb1460ae32d4
anton <tony.potapov@gmail.com>
2016-08-13 19:31:58 +0300

    add onEnter callback to login and register state

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -30,12 +30,22 @@ function($stateProvider, $urlRouterProvider) {
     .state('login', {
       url: '/login',
       templateUrl: 'auth/_login.html',
-      controller: 'AuthCtrl'
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
     })
     .state('register', {
       url: '/register',
       templateUrl: 'auth/_register.html',
-      controller: 'AuthCtrl' 
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }] 
     });
 
   $urlRouterProvider.otherwise('home');
