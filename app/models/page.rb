9d8175a3ce6ef1838702f2dbda1e61408bc41f2b
Денис <denys.potapov@gmail.com>
2016-08-16 16:47:19 +0300

    add load from page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,16 @@
 class Page < ActiveRecord::Base
   
+  def self.path_join(path)
+  	File.join(Rails.configuration.x.data.text_path, path)
+  end
+
   def save_to_file()
-  	File.open(File.join(Rails.configuration.x.data.text_path, path), 'w') do |f|
-  		f.puts text
-	end
+  	File.write(self.class.path_join(path), text)
+  end
+
+  def self.load_from_file(path)
+  	page = where(path: path).first_or_create
+  	page.text = File.read(self.path_join(path))
+  	page.save()
   end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
+  def path_join(path)
+    File.join(Rails.configuration.x.data.text_path, path)
+  end
+
+  after(:all) do
+  	Dir[path_join("**/*.txt")].each do |file|
+      File.delete(file)
+    end
+  end
+
   it "saves text to file" do
   	page = Page.create(path: "1/2.txt", text: "book 1 page 1")
   	page.save_to_file()
-  	File.read(File.join(Rails.configuration.x.data.text_path, "1/2.txt")).should match "book 1 page 1"
+  	expect(File.read(path_join("1/2.txt"))).to eq("book 1 page 1")
+  end
+
+  it "loads file to new page" do
+  	File.write(path_join("1/3.txt"), "book 1 page 3")
+  	Page.load_from_file("1/3.txt")
+  	expect(Page.where(path: "1/3.txt").first.text).to eq("book 1 page 3")
+  end
+
+  it "updates page text from file" do
+  	Page.create(path: "1/4.txt", text: "old text")
+  	File.write(path_join("1/4.txt"), "book 1 page 4")
+  	Page.load_from_file("1/4.txt")
+  	expect(Page.where(path: "1/4.txt").first.text).to eq("book 1 page 4")  	
   end
 end
