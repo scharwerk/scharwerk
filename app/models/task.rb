d8570dc71f6e32456e1e6161e2b0bd6bcd29cb1b
anton <tony.potapov@gmail.com>
2016-09-21 12:02:57 +0300

    fix bug in enum declaration

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,7 +14,7 @@
 class Task < ActiveRecord::Base
   belongs_to :user
 
-  # enum status: {new: 0, in_progress: 1, done: 2, commited: 3}
-  # enum stage: {recognized: 0, first_proof: 1, second_proof: 2}
-  # enum part: {book_1: 1, book_2: 2, book_3_1: 3, book_3_2: 4}  
+  enum status: {not_started: 0, in_progress: 1, done: 2, commited: 3}
+  enum stage: {recognized: 0, first_proof: 1, second_proof: 2}
+  enum part: {book_1: 1, book_2: 2, book_3_1: 3, book_3_2: 4}  
 end
