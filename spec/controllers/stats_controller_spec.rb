7fb52417826fd9f6918b1f2f3d8dd9c3e5079114
denys <denys.potapov@gmail.com>
2016-10-26 18:02:35 +0300

    fix test

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe StatsController, type: :controller do
   describe 'GET #tasks' do
     it 'returns stats' do
-      Task.create(stage: :first_proof, status: :not_started)
-      Task.create(stage: :first_proof, status: :in_progress)
+      Task.create(stage: :first_proof, status: :free)
+      Task.create(stage: :first_proof, status: :active)
       get :tasks, stage: 'first_proof', format: :json
       parsed_body = JSON.parse(response.body)
       expect(parsed_body['0']).to eq(1)
