bdd746409a48a0b02de76fba904444367b95f581
denys <denys.potapov@gmail.com>
2016-10-26 17:39:03 +0300

    add pages to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,16 @@ class TasksController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    !current_user.active_task && (render text: 'not found', status: :not_found) && return
+    is_task_missing && return
     respond_with current_user.active_task
   end
 
   # assign free task to user
   def create
-    current_user.active_task && (render text: 'already assiged', status: :bad_request) && return
+    if current_user.active_task
+      render text: 'already assiged', status: :bad_request
+      return
+    end
 
     stage = params['stage']
     task = Task.where(stage: Task.stages[stage]).free.first
@@ -19,13 +22,22 @@ class TasksController < ApplicationController
 
   # mark task as completed
   def update
-    !current_user.active_task && (render text: 'not found', status: :not_found) && return
+    task_missing && return
   end
 
   # free task from user
   def destroy
-    !current_user.active_task && (render text: 'already assiged',status: :not_found) && return
+    task_missing && return
     current_user.active_task.release
     render text: 'released', status: :ok
+  end
+
+  private
+
+  def task_missing
+    current_user.active_task && return
+
+    render text: 'not found', status: :not_found
+    true
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,7 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   it 'shows text' do
-    info = Task.create(stage: :first_proof, part: :book_1).info
-    expect(info[:description]).to eq('Перша корректура. Капітал. Том І.')
+    task = Task.create(stage: :first_proof, part: :book_1)
+    expect(task.description).to eq('Перша корректура. Капітал. Том І.')
+  end
+
+  it 'calculates progress' do
+    task = Task.create
+    task.pages.create(status: :free)
+    task.pages.create(status: :free)
+    task.pages.create(status: :done)
+    expect(task.progress).to eq(BigDecimal.new(1) / 3)
+  end
+
+  it 'show curent page' do
+    task = Task.create
+    task.pages.create(status: :done)
+    task.pages.create(status: :done)
+    current = task.pages.create(status: :free)
+    expect(task.current_page).to eq(current)
   end
 end
