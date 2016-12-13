07a4bc0d193e548ee64f2e751c3dd8c292c8acc5
denys <denys.potapov@gmail.com>
2016-12-13 13:27:06 +0200

    update stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,19 @@
 # add top level class documentation
 class StatsController < ApplicationController
+  # Rails black magic
+  # Should return hash of hashes of tasks stats
+  # ex {
+  #   "first_proof" : {"active": 10, "completed": 90},
+  #   "second_proof" : {"active": 0, "completed": 100}
+  # }
   def tasks
-    stage = params['stage']
-    respond_with Task.where(stage: Task.stages[stage]).group(:status).count
+    result = {}
+    Task.stages.each { |s, _| result[s] = {} }
+    Task.group(:status, :stage).count.each do |ss, count|
+      status = Task.statuses.key(ss[0])
+      stage = Task.stages.key(ss[1])
+      result[stage][status] = count
+    end
+    respond_with result
   end
 end
--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe StatsController, type: :controller do
   describe 'GET #tasks' do
     it 'returns stats' do
+      Task.create(stage: :test, status: :active)
       Task.create(stage: :first_proof, status: :free)
       Task.create(stage: :first_proof, status: :active)
       get :tasks, stage: 'first_proof', format: :json
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body['0']).to eq(1)
-      expect(parsed_body['1']).to eq(1)
+      expect(parsed_body['first_proof']['active']).to eq(1)
+      expect(parsed_body['first_proof']['free']).to eq(1)
+      expect(parsed_body['test']['active']).to eq(1)
     end
   end
 end
