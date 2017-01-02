c09211cf67d92d4f8ce5650ec4eaf270fc910fad
denys <denys.potapov@gmail.com>
2017-01-02 23:14:00 +0200

    js cleanup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -15,17 +15,6 @@ angular.module('scharwerk',
 function($stateProvider, $urlRouterProvider, ezfbProvider) {
 
   $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: 'home/_home.html',
-      controller: 'MainCtrl',
-      resolve: {
-        postPromise: ['posts', function(posts){
-          return posts.getAll();
-        }]
-      }
-    })
-
     .state('proof', {
       url: '/proof',
       templateUrl: 'proof/_proof.html',
@@ -44,38 +33,6 @@ function($stateProvider, $urlRouterProvider, ezfbProvider) {
           return tasks.getCurrent();
         }]
       }
-    })
-
-    .state('posts', {
-      url: '/posts/{id}',
-      templateUrl: 'posts/_posts.html',
-      controller: 'PostsCtrl',
-      resolve: {
-        post: ['$stateParams', 'posts', function($stateParams, posts) {
-          return posts.get($stateParams.id);
-        }]
-      }
-    })
-
-    .state('login', {
-      url: '/login',
-      templateUrl: 'auth/_login.html',
-      controller: 'AuthCtrl',
-      onEnter: ['$state', 'Auth', function($state, Auth) {
-        Auth.currentUser().then(function (){
-          $state.go('home');
-        })
-      }]
-    })
-    .state('register', {
-      url: '/register',
-      templateUrl: 'auth/_register.html',
-      controller: 'AuthCtrl',
-      onEnter: ['$state', 'Auth', function($state, Auth) {
-        Auth.currentUser().then(function (){
-          $state.go('home');
-        })
-      }] 
     });
 
   $urlRouterProvider.otherwise('index');
