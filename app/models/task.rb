d28a012126ef4d1792f6dbf3e2df55d044397b7c
Chmen <tony.potapov@gmail.com>
2017-01-09 12:52:04 +0200

    add franko: part to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -22,7 +22,7 @@ class Task < ActiveRecord::Base
 
   enum status: { free: 0, active: 1, done: 2, commited: 3 }
   enum stage: { test: 0, first_proof: 1, second_proof: 2 }
-  enum part: { book_1: 1, book_2: 2, book_3_1: 3, book_3_2: 4 }
+  enum part: { book_1: 1, book_2: 2, book_3_1: 3, book_3_2: 4, franko: 5 }
 
   def description
     I18n.t(stage) + ' ' + I18n.t(part)
