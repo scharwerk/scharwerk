105bf2a371bdb05c5ff00dde9aa61f0b5a962ac1
Chmen <tony.potapov@gmail.com>
2017-01-12 19:40:19 +0200

    remove duplicate migration

--- db/migrate/20170112171825_fixup_add_path_index_to_page.rb
+++
@@ -1,9 +0,0 @@
-require_relative '20160816140524_add_path_index_to_page'
-
-class FixupAddPathIndexToPage < ActiveRecord::Migration
-  def change
-  	revert AddPathIndexToPage
-
-  	add_index(:pages, :path)
-  end
-end
