e6d00fd3f5b578badc76f83a568dc358c7a47d77
denys <denys.potapov@gmail.com>
2017-01-21 12:57:58 +0200

    pass tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
   describe 'POST #pages' do
-    it 'saves page' do
+    it 'saves page and proceeds to next' do
       user = User.create
       sign_in user
       task = Task.create(stage: :test, status: :active, user: user)
-      page1 = task.pages.create(status: :free)
-      page2 = task.pages.create(status: :free)
+      page1 = task.pages.create(status: :free, path: '1')
+      page2 = task.pages.create(status: :free, path: '1')
 
-      put :update, id: page1.id, text: 'new text', format: :json
+      put :update, id: page1.id, text: 'new text', done: true, format: :json
       expect(Page.find(page1.id).text).to eq('new text')
 
       parsed_body = JSON.parse(response.body)
-      expect(parsed_body['id']).to eq(page2.id)
+      expect(parsed_body['current_page']['id']).to eq(page2.id)
     end
   end
 
@@ -22,7 +22,7 @@ RSpec.describe PagesController, type: :controller do
       user = User.create
       sign_in user
       task = Task.create(stage: :test, status: :active, user: user)
-      page = task.pages.create(status: :free)
+      page = task.pages.create(status: :free, path: '1')
 
       get :show, id: page.id, format: :json
       parsed_body = JSON.parse(response.body)
