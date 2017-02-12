c9f0092be07248f58659f7247c837fa30983d17e
denys <denys.potapov@gmail.com>
2017-02-12 18:27:23 +0200

    add top list

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -16,4 +16,17 @@ class StatsController < ApplicationController
     end
     respond_with result
   end
+
+  def top
+    result = { top: [], going: [] }
+    User.all.each do |user|
+      if user.tasks_done > 0
+        result[:top] << { name: user.name, tasks: user.tasks_done }
+      elsif user.active_task
+        result[:going] << { name: user.name, pages: user.pages_done }
+      end
+    end
+    result[:top].sort_by! { |k| -k[:tasks] }
+    respond_with result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   post 'login' => 'users#login'
   get 'stats/tasks'
+  get 'stats/top'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
