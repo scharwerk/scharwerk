3527d9e73f3cc90171627b95323442386c5acd6a
anton <tony.potapog@gmail.com>
2018-02-08 20:56:05 +0200

    Move restricted_user_id from task to restriction

--- db/migrate/20180204115304_add_restricted_users_to_tasks.rb
+++ db/migrate/20180204115304_add_restricted_users_to_tasks.rb
@@ -2,13 +2,21 @@ require_relative '20180107172830_add_restricted_user_to_tasks'
 
 class AddRestrictedUsersToTasks < ActiveRecord::Migration
   def up
-    revert AddRestrictedUserToTasks
-
     create_table :restrictions do |t|
       t.belongs_to :user, index: true
       t.belongs_to :task, index: true
     end
 
+    Task.all.each do |task|
+      if !task.restricted_user_id.nil?
+        restriction = Restriction.new
+        restriction.task_id = task.id
+        restriction.user_id = task.restricted_user_id
+        restriction.save
+      end
+    end
+
+    revert AddRestrictedUserToTasks
   end
 
   def down
