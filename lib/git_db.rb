1831fbc550d932b4a18c33935cdb32af80f2dcb7
denys <denys.potapov@gmail.com>
2018-02-18 12:30:20 +0200

    estract git

--- lib/git_db.rb
+++ lib/git_db.rb
@@ -1,18 +1,30 @@
+# store texts in git
 class GitDb
+  def self.path
+    Rails.configuration.x.data.git_path.to_s
+  end
 
-  def initialize()
-  	path = Rails.configuration.x.data.git_path.to_s
-    @git = Git.open(path)
+  def initialize
+    @git = Git.open(self.class.path)
   end
 
   def commit(pathes, message)
     @git.add(pathes)
     @git.commit(message)
-    return :ok
+    return :commited
   rescue Git::GitExecuteError => e
     return :unchanged if e.message.include? 'nothing to commit'
 
     raise
   end
 
+  def latest_commit(path)
+    @git.log(1).object(path).first.to_s
+  end
+
+  def last_changes(path)
+    commit = latest_commit(path)
+    lines = @git.lib.diff_full(commit + '^!', '--word-diff=porcelain')
+    lines.split(/\n+/).grep(/^[\+\-][^\+\-]/)
+  end
 end
---
+++ spec/lib/git_db_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe GitDb do
+  before(:all) do
+    FileUtils.mkdir_p(File.join(GitDb.path, '/test/'))
+    Git.init(GitDb.path)
+  end
+
+  after(:all) do
+    FileUtils.rm_rf(GitDb.path)
+  end
+
+  def write_text(path, text)
+    full_path = File.join(GitDb.path, path)
+    File.write(full_path, text)
+    full_path
+  end
+
+  it 'show last commit' do
+    g = Git.open(GitDb.path)
+    p = write_text('test/1.txt', 'text')
+    git = GitDb.new
+    git.commit(p, 'message')
+
+    expect(git.latest_commit(p)).to eq(g.log[0].to_s)
+  end
+
+  it 'show number of changes' do
+    git = GitDb.new
+    p = write_text('test/1.txt', "one two next\nthree")
+    git.commit(p, 'message')
+    p = write_text('test/1.txt', "onetwo next\nfour")
+    git.commit(p, 'message')
+    changes = git.last_changes(p)
+
+    expect(changes[0]).to eq('-one two')
+    expect(changes.count).to eq(4)
+  end
+end
