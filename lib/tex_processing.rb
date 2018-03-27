ec9d6dae764941e991fa0bf978dddd68b50d1824
anton <tony.potapog@gmail.com>
2018-03-27 21:49:26 +0300

    fix bug in footnote? method

--- lib/tex_processing.rb
+++ lib/tex_processing.rb
@@ -7,6 +7,15 @@ class TexProcessing
 
   def self.footnote?(paragraph)
     return false if paragraph.match(/^\d\./) || paragraph.match(/^\d\d\./)
-    return true if paragraph.match(/^\d/) || paragraph.match(/^*/)
+    return true if paragraph.match(/^\d/) || paragraph.match(/^\*/)
+    false
+  end
+
+  def self.footnotes_array(text)
+    result = []
+    text.split(/\n\n/).each do |paragraph|
+      result << paragraph if footnote?(paragraph)
+    end
+    result
   end
 end
--- spec/lib/tex_processing_spec.rb
+++ spec/lib/tex_processing_spec.rb
@@ -21,6 +21,20 @@ RSpec.describe TexProcessing do
   # end
 
   describe '.footnote?' do
+    context 'with ordinary paragraph' do
+      it 'return false' do
+        paragraph = 'Остання форма, форма С, дає, нарешті, товаровому світові'\
+                    'загальносуспільну відносну форму вартости тому й остільки,'\
+                    'що й оскільки всі належні до нього товари, за одним-однісіньким'\
+                    'винятком, є виключені з загальної еквівалентної форми. Тому'\
+                    'один товар, полотно, перебуває у формі безпосередньої вимінности'\
+                    'на всі інші товари, або в безпосередньо суспільній формі, тому'\
+                    'й остільки, що й оскільки всі інші товари не перебувають у такій'\
+                    'формі.24'
+        expect(TexProcessing.footnote?(paragraph)).to eq false
+      end
+    end
+
     context 'with footnote paragraphs' do
       it 'return true with number' do
         # TODO: how to identify footnote thet, started on previous page?
@@ -52,4 +66,20 @@ RSpec.describe TexProcessing do
       end
     end
   end
+  # describe '.footnotes_array' do
+  #   it 'return array of footnote' do
+  #     source = read_file('footnotes.simple.txt')
+  #     first_footnote = '18   У деякому відношенні з людиною справа стоїть так, як із товаром.'\
+  #                      'Через те, що вона родиться на світ ані з дзеркалом, ані як фіхтівський'\
+  #                      'філософ: «Я є я», то людина спершу видивляється в іншу людину, як у'\
+  #                      'дзеркало. Лише через відношення до людини Павла як до подібного до'\
+  #                      'себе, людина Петро відноситься й до себе самої як до людини. Але тим'\
+  #                      'самим і Павло з шкурою й волоссям, у його Павловій тілесності, стає для'\
+  #                      'нього за форму виявлення роду «людина».'
+  #     second_footnote = '* Париж таки вартий служби божої. Ред.'
+
+  #     result = [first_footnote, second_footnote]
+  #     expect(TexProcessing.footnotes_array(source)).to eq result
+  #   end
+  # end
 end
