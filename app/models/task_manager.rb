eab48b49a8a6931f5a8e6b86bfb7d4b450fb1d18
denys <denys.potapov@gmail.com>
2018-04-12 00:44:08 +0300

    load images on task create

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -1,12 +1,14 @@
 # Class for managing tasks
 class TaskManager
-
   # generate tasks for markup
   def self.generate_task3(pattern, part)
     Dir[Task.tex_path(pattern)].sort.collect do |tex_file|
-        path = tex_file[Task.tex_path('').length..-5]
-        Task.create(stage: :markup, part: part, path: path)
+      path = tex_file[Task.tex_path('').length..-5]
+      t = Task.create(stage: :markup, part: part, path: path)
+      t.pages = File.basename(path).split('_').collect do |p|
+        Page.create(path: File.join(File.dirname(path), p))
+      end
+      t
     end
   end
-
 end
--- spec/models/task_manager_spec.rb
+++ spec/models/task_manager_spec.rb
@@ -14,10 +14,13 @@ RSpec.describe TaskManager do
   end
 
   it 'generate tasks' do
-    make_tex('test/1.tex', '\\latex')
+    make_tex('test/0001_0002.tex', '\\latex')
     task = TaskManager.generate_task3('test/*', 'franko')[0]
-    expect(task.path).to eq('test/1')
+    expect(task.path).to eq('test/0001_0002')
     expect(task.tex).to eq('\\latex')
     expect(task.part).to eq('franko')
+
+    expect(task.pages.count).to eq 2
+    expect(task.pages[0].image).to eq '/files/images/test/0001.jpg'
   end
 end
