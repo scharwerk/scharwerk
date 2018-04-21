2914cc51df03b9528ee2b48c0b4d7ba848ab30f0
denys <denys.potapov@gmail.com>
2018-04-22 02:02:16 +0300

    i join files

--- lib/joiner.rb
+++ lib/joiner.rb
@@ -1,8 +1,30 @@
 # This is a class for joining files
 class Joiner
-  def self.join(path, config, index)
-    parts = config.split("\n\n")
+  def self.read_file(path, page, index)
+    name = page.delete('*-')
 
+    text = File.read(File.join(path, name))
+    text = text.gsub(/\s+\Z/, '').gsub(/\A\s+/, '')
+
+    flags = page.slice(name.length..-1)
+    index = name.split('.')[0]
+    return text, flags, index
+  end
+
+  def self.join(path, config, index)
+    config.split("\n\n").each do |part|
+      text = ''
+      name = ''
+      complex = false
+      part.split("\n").each do |page|
+        t, f, i = self.read_file(path, page, index)
+        text += t
+        name += '_' + i
+        complex = (complex or (f.index('*') != nil))
+      end
+      filename =name + (complex ? 'c' : '') + '.tex'
+      File.write(File.join(path, filename), text)
+    end
   end
 
   def self.remove_end(text1, text2)
