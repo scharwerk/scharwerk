60110aa8ce5e2bbe410c854fc1e5f127a61236f9
denys <denys.potapov@gmail.com>
2018-05-16 22:33:37 +0300

    add delayed status

--- app/models/task.rb
+++ app/models/task.rb
@@ -21,7 +21,7 @@ class Task < ActiveRecord::Base
 
   enum status: { free: 0, active: 1, done: 2,
                  commited: 3, error: 4, unchanged: 5,
-                 reproof: 6 }
+                 reproof: 6, delaуed: 7 }
   enum stage: { test: 0, first_proof: 1, second_proof: 2,
                 markup: 3, markup_complex: 4 }
   enum part: { book_1: 1, book_2: 2, book_3_1: 3, book_3_2: 4, franko: 5 }
