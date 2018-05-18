b35a27b9147e3c107c085769f31f23db3385faef
denys <denys.potapov@gmail.com>
2018-05-18 23:36:15 +0300

    update counters

--- app/assets/javascripts/stages/stages.js
+++ app/assets/javascripts/stages/stages.js
@@ -8,8 +8,8 @@ angular.module('scharwerk')
     return $http.get('/stats/tasks.json').success(function(data){
       angular.forEach(data, function(stage, key) {
         // temporary fix
-        // add 500 to markup and 100 to markup complex
-        stage.total = stage.total + ((key == 'markup') ? 500 : 100);
+        // add 254 to markup and 35 to markup complex
+        stage.total = stage.total + ((key == 'markup') ? 254 : 35);
         // check if undefined and summ
         finished = (stage.commited || 0) + (stage.unchanged || 0) + (stage.reproof || 0);
         console.log(finished);
