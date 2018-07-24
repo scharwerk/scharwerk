bab6090b38b360c2ce36d662c9ef135b5b70fba0
denys <denys.potapov@gmail.com>
2018-07-24 23:01:42 +0300

    remove dirty count fix

--- app/assets/javascripts/stages/stages.js
+++ app/assets/javascripts/stages/stages.js
@@ -7,9 +7,6 @@ angular.module('scharwerk')
   s.getStats = function() {
     return $http.get('/stats/tasks.json').success(function(data){
       angular.forEach(data, function(stage, key) {
-        // temporary fix
-        // add 254 to markup and 35 to markup complex
-        stage.total = stage.total + ((key == 'markup') ? 254 : 35);
         // check if undefined and summ
         finished = (stage.commited || 0) + (stage.unchanged || 0) + (stage.reproof || 0);
         console.log(finished);
