10f358f975157c39548b69f087ea44907a4924ca
denys <denys.potapov@gmail.com>
2018-09-10 01:32:18 +0300

    add useful scripts

--- scripts.rb
+++ scripts.rb
@@ -5,3 +5,7 @@ User.all.each do |u|
   puts "ok, %s, %s" % [u.total_pages_done, g]
   [u.total_pages_done, g]
 end
+
+# count changes by task
+totals = Hash.new(0)
+Task.markup.commited.each { |t| totals[t.line_diff_count] += 1 }
